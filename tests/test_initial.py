c66f4ddb306aacf5e4562a0bc48dc70099f11c42
Eric Rosas <ericdrosas@gmail.com>
2022-12-28 15:29:23 -0700

    Quick test same as before EPO-7345

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -13,4 +13,4 @@ def test_create_new_batch():
     assert main.create_new_batch(10000, 20000) > 0
 
 def test_check_batch_status():
-    assert len(main.check_batch_status(10000, 20000)) > 0
+    assert len(main.check_batch_status(10000, 20000)) == 10
