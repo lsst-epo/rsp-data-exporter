331679ba28bc8f04fbcd92ab872baac0b017828d
Eric Rosas <ericdrosas@gmail.com>
2023-01-11 14:00:27 -0700

    Added explicit multi-step owner/project/batch flow EPO-7449

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 _VENDOR_PROJECT_ID = 99999
 _TEST_EMAIL = "some@fake.email"
-_OWNER_ID = 0
+# _OWNER_ID = 0
 _PROJECT_ID = 0
-_BATCH_ID = 0
+# _BATCH_ID = 0
 
 # If pytest is working fine then this test will always execute successfully
 def test_sanity_check():
@@ -25,18 +25,19 @@ def test_lookup_owner_record():
 
 # Project record tests
 def test_create_new_project():
-    global _OWNER_ID, _VENDOR_PROJECT_ID
+    _OWNER_ID = main.create_new_owner_record(_TEST_EMAIL)
     _PROJECT_ID = main.create_new_project_record(_OWNER_ID, _VENDOR_PROJECT_ID)
     assert _PROJECT_ID == 1
 
 def test_lookup_project_record():
-    global _VENDOR_PROJECT_ID
-    assert main.lookup_project_record(_VENDOR_PROJECT_ID) == 1
+    assert main.lookup_project_record(_VENDOR_PROJECT_ID) == 2
 
 # Batch record tests
 def test_create_new_batch():
+    _OWNER_ID = main.create_new_owner_record(_TEST_EMAIL)
+    _PROJECT_ID = main.create_new_project_record(_OWNER_ID, _VENDOR_PROJECT_ID)
     _BATCH_ID = main.create_new_batch(_PROJECT_ID, _VENDOR_PROJECT_ID)
     assert _BATCH_ID == 1
 
 def test_check_batch_status():
-    assert len(main.check_batch_status(_PROJECT_ID, _VENDOR_PROJECT_ID)) == 1
+    assert len(main.check_batch_status(_PROJECT_ID, _VENDOR_PROJECT_ID)) == 2
