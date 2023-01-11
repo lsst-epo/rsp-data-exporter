f4e956682c2dfd5a7d7b8a0d6d0b713394b77011
Eric Rosas <ericdrosas@gmail.com>
2023-01-11 14:09:08 -0700

    Removed global vars from test suite EPO-7449

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -5,39 +5,36 @@ from dotenv import load_dotenv
 from rsp_data_exporter import main
 load_dotenv()
 
-_VENDOR_PROJECT_ID = 99999
-_TEST_EMAIL = "some@fake.email"
-# _OWNER_ID = 0
-_PROJECT_ID = 0
-# _BATCH_ID = 0
-
 # If pytest is working fine then this test will always execute successfully
 def test_sanity_check():
     assert main.check_test_only_var() == True
 
 # Owner record tests
 def test_create_owner_record():
-    _OWNER_ID = main.create_new_owner_record(_TEST_EMAIL)
+    _OWNER_ID = main.create_new_owner_record("fake@email.com")
     assert _OWNER_ID == 1
 
 def test_lookup_owner_record():
-    assert main.lookup_owner_record(_TEST_EMAIL) == 1
+    assert main.lookup_owner_record("fake@email.com") == 1
 
 # Project record tests
 def test_create_new_project():
-    _OWNER_ID = main.create_new_owner_record(_TEST_EMAIL)
-    _PROJECT_ID = main.create_new_project_record(_OWNER_ID, _VENDOR_PROJECT_ID)
+    _OWNER_ID = main.create_new_owner_record("fake@email.org")
+    _PROJECT_ID = main.create_new_project_record(_OWNER_ID, 99999)
     assert _PROJECT_ID == 1
 
 def test_lookup_project_record():
-    assert main.lookup_project_record(_VENDOR_PROJECT_ID) == 2
+    assert main.lookup_project_record(99999) == 2
 
 # Batch record tests
 def test_create_new_batch():
-    _OWNER_ID = main.create_new_owner_record(_TEST_EMAIL)
-    _PROJECT_ID = main.create_new_project_record(_OWNER_ID, _VENDOR_PROJECT_ID)
-    _BATCH_ID = main.create_new_batch(_PROJECT_ID, _VENDOR_PROJECT_ID)
+    _OWNER_ID = main.create_new_owner_record("fake@email.net")
+    _PROJECT_ID = main.create_new_project_record(_OWNER_ID, 99999)
+    _BATCH_ID = main.create_new_batch(_PROJECT_ID, 99999)
     assert _BATCH_ID == 1
 
 def test_check_batch_status():
-    assert len(main.check_batch_status(_PROJECT_ID, _VENDOR_PROJECT_ID)) == 2
+    _OWNER_ID = main.create_new_owner_record("fake@email.io")
+    _PROJECT_ID = main.create_new_project_record(_OWNER_ID, 99999)
+    _BATCH_ID = main.create_new_batch(_PROJECT_ID, 99999)
+    assert len(main.check_batch_status(_PROJECT_ID, 99999)) == 2
