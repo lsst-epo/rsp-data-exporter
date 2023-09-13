30ba6e448019da23c834bf897255292d32bdb7cd
Eric Rosas <ericdrosas@gmail.com>
2023-09-13 15:38:16 -0600

    Removed debug logging EPO-8460

--- src/rsp_data_exporter/audit_service.py
+++ src/rsp_data_exporter/audit_service.py
@@ -40,14 +40,9 @@ def fetch_audit_records(vendor_project_id):
 
 def insert_audit_records(vendor_project_id, validator):
     audit_records = []
-
-    logger.log_text("About to loop over validator.mapped_manifest in insert_audit_records()")
     object_ids = []
     for key in validator.mapped_manifest:
-        logger.log_text("-- looking up key: " + key)
-        logger.log_text(str(validator.mapped_manifest[key]))
         if "objectId" in validator.mapped_manifest[key]:
-            logger.log_text("-- " + str(validator.mapped_manifest[key]))
 
             object_id = validator.mapped_manifest[key]["objectId"]
             object_ids.append(object_id)
@@ -91,14 +86,10 @@ def audit_object_ids(object_ids, vendor_project_id):
 
     db.close()
 
-    logger.log_text("about to loop over audit records and get project name")
     audit_response = []
     for row in audit_aggr:
-        logger.log_text("logging row:")
-        logger.log_text(str(row))
         vendor_project_name = get_vendor_project_name(row[0])
         if vendor_project_name is not None:
-            logger.log_text("vendor_project_name not null!")
             perc_match = float(row[1]) / len(object_ids)
             audit_response.append("The dataset you curated has a " + str(perc_match) + " object ID match on another Zooniverse project: " + vendor_project_name)
 
@@ -108,11 +99,7 @@ def get_vendor_project_name(vendor_project_id):
     logger.log_text("about to lookup project name in get_vendor_project_name")
     try:
         project = Project.find(id=vendor_project_id)
-        logger.log_text("about to log project.raw")
-        logger.log_text(str(project.raw))
         project_name = project.raw["display_name"]
-        logger.log_text("logging project_name:")
-        logger.log_text(project_name)
         return project_name
     except Exception as e:
         if "Could not find project with id" in e.__str__():
